890b432bc887803aab6773fd53f983dc9a9f22fe
Adriatticus <4CorderJ@gmail.com>
2024-09-01 16:43:44 -0400

    two new javaScript files for learning a1115

---
+++ hello.js
@@ -0,0 +1,13 @@
+// prints two things at the same time to the console 
+console.log('hello, world!', 'how\'s it going?'); // <--Commander
+
+//   This is me training.
+//   I still have hardly any idea what's going on. 
+//   All I know right now is that i'm doing some kind of multi line comment
+
+// I think i'm making a box
+let firstName = "Michael";
+let lastName = "Hartl";
+
+// This is how I back tick that a$$ up
+// `${fullName} has backed that a$$ up!`
